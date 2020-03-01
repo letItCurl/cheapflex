a43a5a8aae201b0dd0e8ad683bb1a7839a2ddb28
rollpz <rolandlopez.developer@gmail.com>
2020-03-01 11:02:49 +0100

    spinner donne

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {createGlobalStyle} from 'styled-components'
+
 import Header from './elements/Header'
 import Home from './Home'
 
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}`
+
 const App = () => (<>
-<div><Header/><Home></Home></div>
+    <>
+        <Header/>
+        <Home/>
+        <GlobalStyle/>
+    </>
 </>) 
 
 export default App;
